34dc126652156c7da5993038ee22e82ee575874a
caioragazzini <caiovrragazzini@gmail.com>
2021-01-24 11:16:27 -0300

    first commit

---
+++ IOASYS-IMDb/Authentication/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOASYS_IMDb.Authentication
+{
+    public class Response
+    {
+    }
+}
